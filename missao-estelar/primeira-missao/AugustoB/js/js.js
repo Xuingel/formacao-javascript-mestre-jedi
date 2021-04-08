8eb46e0ebf8df09a0844c126d2051367686aaef1
Xuingel <augustinhob1@gmail.com>
2021-04-08 12:58:58 -0300

    all

---
+++ missao-estelar/primeira-missao/AugustoB/js/js.js
@@ -0,0 +1,11 @@
+function eventoOnload() {
+
+    console.log('evento onload disparado')
+}
+
+var missao="MISSÃO ESTELAR JS em execução"
+
+function eventoONclick(){
+
+    console.log("missaoestelar js realizada com sucesso")
+}
---
+++ missao-estelar/primeira-missao/AugustoB/readme.md
@@ -0,0 +1,1 @@
+tecnologia preferida: Computador, smartphone
